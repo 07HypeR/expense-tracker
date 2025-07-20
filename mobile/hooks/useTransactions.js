d6a45a06e04db0e56ff5ea7f8fb59586cada3447
Abhik Baidya <devStoic07@gmail.com>
2025-07-20 23:48:51 +0530

    API changes

--- mobile/hooks/useTransactions.js
+++ mobile/hooks/useTransactions.js
@@ -1,8 +1,6 @@
 import { useCallback, useState } from "react";
 import { Alert } from "react-native";
-
-const API_URL = "https://expense-api-iemj.onrender.com/api";
-// const API_URL = "http://localhost:5001/api";
+import { API_URL } from "../constants/api";
 
 export const useTransactions = (userId) => {
   const [transactions, setTransactions] = useState([]);
