c6c86d5ea70a4c1249f45971553239729be378c9
Abhik Baidya <devStoic07@gmail.com>
2025-07-21 16:12:54 +0530

    Api Changes

--- mobile/constants/api.js
+++ mobile/constants/api.js
@@ -1,2 +1,7 @@
+import { Platform } from "react-native";
+
 export const API_URL = "https://expense-api-iemj.onrender.com/api";
-// export const API_URL = "http://localhost:5001/api";
+// export const API_URL =
+//   Platform.OS === "ios"
+//     ? "http://localhost:5001/api"
+//     : "http://10.0.2.2:5001/api";
